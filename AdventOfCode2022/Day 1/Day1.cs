318993b714bf1466ca0a73906305f810f3d3d909
Jason Clift <jason@pulsegroupb2b.com>
2022-12-01 21:01:07 -0800

    Initial Commit, and Day 1 Solution

---
+++ AdventOfCode2022/Day 1/Day1.cs
@@ -0,0 +1,49 @@
+﻿namespace AdventOfCode2022;
+
+public class Day1
+{
+    public Dictionary<int, int> ElfsInventory { get; set; }
+
+    public Day1()
+    {
+        this.ElfsInventory = new Dictionary<int, int>();
+    }
+
+    public async Task GetData()
+    {
+        var tmp = new FileFunctions();
+        var data = await tmp.ReadFile("Day 1/InputData.txt");
+        if (!string.IsNullOrEmpty(data))
+        {
+            this.PopulateData(data);
+        }
+    }
+
+    private void PopulateData(string inputData)
+    {
+        int elfs = 1;
+        var split = inputData.Split("\r\n\r\n").ToList();
+        foreach (var s in split)
+        {
+            var set = s.Split("\r\n");
+            var inventory = set.Sum(s1 => Convert.ToInt32(s1));
+            this.ElfsInventory.Add(elfs, inventory);
+            //Console.WriteLine($"Elf {elfs} has {inventory}");
+            elfs++;
+        }
+    }
+
+    public KeyValuePair<int, int> GetElfWithMostFood()
+    {
+        return ElfsInventory.MaxBy(kvp => kvp.Value);
+    }
+
+    public int GetTop3ElfsFood()
+    {
+        var topValues = this.ElfsInventory.Values
+            .OrderByDescending(x => x)
+            .Take(3)
+            .ToArray();
+        return topValues.Sum();
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using AdventOfCode2022;
+
+var Day1Part1 = new Day1();
+
+await Day1Part1.GetData();
+var elf = Day1Part1.GetElfWithMostFood();
+Console.WriteLine($"Elf {elf.Key} has {elf.Value}");
+Console.WriteLine($"Top 3 Elfs have {Day1Part1.GetTop3ElfsFood()} Food");
