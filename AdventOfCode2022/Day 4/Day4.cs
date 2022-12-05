3ceb8ce3db0999133180491cde6f8833c977c89b
Jason Clift <jason@pulsegroupb2b.com>
2022-12-04 22:03:09 -0800

    Day 4 complete, really struggled with this one, in the end I had to look up a solution do not like the end with the <= would have love to figure out Enumerable.Range(set1Start, set1End-set1Start);

---
+++ AdventOfCode2022/Day 4/Day4.cs
@@ -0,0 +1,45 @@
+﻿namespace AdventOfCode2022.Day_4;
+
+public class Day4
+{
+    private string? RawData { get; set; }
+    public int Overlap { get; set; }
+
+    public async Task GetInputData()
+    {
+        var ff = new FileFunctions();
+        this.RawData = await ff.ReadFile("Day 4/InputData.txt");
+    }
+
+    public void ProcessData()
+    {
+        var split = this.RawData.Split("\r\n");
+        foreach (var s in split)
+        {
+            var assignments = s.Split(',');
+            var elf1 = assignments[0].Split('-');
+            var elf2 = assignments[1].Split('-');
+            if (FullyContains(Convert.ToInt32(elf1[0]), Convert.ToInt32(elf1[1]), Convert.ToInt32(elf2[0]),
+                    Convert.ToInt32(elf2[1])))
+            {
+                Overlap++;
+                continue;
+            }
+
+            //Console.WriteLine($"{s} Failed");
+        }
+    }
+
+    public bool FullyContains(int StartRange, int EndRange, int CheckStart, int CheckEnd)
+    {
+        if ((StartRange <= CheckStart && CheckEnd <= EndRange) ||
+            (CheckStart <= StartRange && EndRange <= CheckEnd))
+            return true;
+
+        if ((StartRange <= CheckStart && EndRange >= CheckStart) ||
+            (CheckStart <= StartRange && CheckEnd >= StartRange))
+            return true;
+        
+        return false;
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -4,10 +4,12 @@
 using AdventOfCode2022;
 using AdventOfCode2022.Day_2;
 using AdventOfCode2022.Day_3;
+using AdventOfCode2022.Day_4;
 
 var Day1Part1 = new Day1();
 var Day2 = new Day2();
 var Day3 = new Day3();
+var Day4 = new Day4();
 
 await Day1Part1.GetData();
 var elf = Day1Part1.GetElfWithMostFood();
@@ -25,3 +27,7 @@ await Day3.GetInputData();
 Day3.ParseData();
 Console.WriteLine($"RuckSack Common Items Score is {Day3.GetScoreOfCommonItems()}");
 Console.WriteLine($"RuckSack Badge Score is {Day3.BadgeScore}");
+
+await Day4.GetInputData();
+Day4.ProcessData();
+Console.WriteLine($"Workspace overlap {Day4.Overlap}");
